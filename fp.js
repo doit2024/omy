a2b01aa59dc201d96091bd5bff7ded6103f5f636
niuDazhuang <denglei678@icloud.com>
2018-04-28 11:48:37 +0800

    Update fp.js

--- fp.js
+++ fp.js
@@ -11,6 +11,71 @@ const memoize = f => {
   }
 }
 
+const pp = (p, x) => x[p]
+const up = x => x.toUpperCase()
+const cc = (b, x) => x.concat(b)
+const mh = (reg, x) => x.match(reg)
+const mp = (f, fun) => fun.map(f)
+// props :: s => o => a
+const prop = curry(pp)
+// concat :: a -> a -> a
+const concat = curry(cc)
+// match :: reg -> s -> [a]
+const match = curry(mh)
+// map :: Funtor f => (a -> b) -> f a -> f b
+const map = curry(mp)
+
+/* --------------------------------------------------------------------------------- */
+class Container {
+  constructor (x) {
+    this.__value = x
+  }
+  static of (x) {
+    return new Container(x)
+  }
+  map (f) {
+    return Container.of(f(this.__value))
+  }
+}
+
+/* --------------------------------------------------------------------------------- */
+const a = Container.of('hello')
+const b = Container.of({name: 'woody'})
+const ra = a.map(up)
+const rb = b.map(prop('name')).map(concat(' teng'))
+console.log(ra)
+console.log(rb)
+
+/* --------------------------------------------------------------------------------- */
+class Maybe extends Container {
+  static of (x) {
+    return new Maybe(x)
+  }
+  map (f) {
+    return [null, undefined].includes(this.__value)
+      ? Maybe.of(null)
+      : Maybe.of(f(this.__value))
+  } 
+}
+
+/* --------------------------------------------------------------------------------- */
+const c = Maybe.of('hello woody')
+const d = Maybe.of()
+const rc = c.map(match(/e/ig))
+const rd = d.map(match(/e/ig))
+console.log(rc)
+console.log(rd)
+
+// 常用： 函数可能无法成功返回结果
+const safeHead = xs => Maybe.of(xs[0])
+const getName = compose(map(prop('name')), safeHead, prop('list'))
+const re = getName({list: []})
+const rf = getName({list: [{name: 'tom', age: 25}]})
+console.log(re)
+console.log(rf)
+/* --------------------------------------------------------------------------------- */
+
+
 /////////////////////////////////// 斐波那契 //////////////////////////////////////////
 
 /***
